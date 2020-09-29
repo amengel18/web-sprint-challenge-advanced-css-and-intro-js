f6b5f2255e4e662027dd93f49303e64ecfd535db
Angela Engel <amengel.ae@icloud.com>
2020-09-28 18:19:03 -0600

    Changes

--- index.js
+++ index.js
@@ -254,12 +254,12 @@ console.log(message)
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-  function get20s(arr,str){
+  function get20s(artists,str){
 
   let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].includes(str)) {
-      newArr.push(arr[i]);
+  for (let i = 0; i < artists.length; i++) {
+    if (artists[i].includes(str)) {
+      newArr.push(artists[i]);
     }
   }
     return newArr;
